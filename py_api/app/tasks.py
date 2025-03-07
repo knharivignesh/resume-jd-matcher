b26e613c53e3e30f83ff60441e9772ccd78f4535
Hari Vignesh <harivignesh.n@payoda.com>
2025-03-07 14:35:43 +0530

    Fully functional work with the celery bg task

---
+++ py_api/app/tasks.py
@@ -0,0 +1,19 @@
+from celery import shared_task
+from flask import current_app
+from app.resume_job import ResumeJob
+
+
+@shared_task(ignore_result=False)
+def start_resume_parsing(resume_data: dict) -> dict:
+    """function ap_upgrade_apply_revoke
+    celery task apply or revoke policy for the give wm urls.
+
+    Keyword arguments:
+    argument -- ap_upgrade: ApTaskArgs
+    Return: json success/Fail state and message
+    """
+    try:
+        resume_job = ResumeJob(resume_data.get("job_id"))
+        resume_job.parse_resume()
+    except Exception as error:
+        current_app.logger.error(f"Error in parsing resume: {error}")
